92696c4868bb9dee7e740314bf930dafe6774840
Frédéric <desseaux_frederic@yahoo.fr>
2021-05-20 20:19:00 +0200

    V1-index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+// Import packages
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+app.use(helmet());
+
+// Connexion BDD
+mongoose.connect("mongodb://localhost/airbnb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Import Routes
+const userRoutes = require("./routes/user");
+app.use(userRoutes);
+
+// Main page
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Airbnb API" });
+});
+
+// Other page
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
